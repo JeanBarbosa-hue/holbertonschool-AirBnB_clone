993154c398e7929a93e159533114b61f5657ad68
JeanBarbosa-hue <jeanmaldy@gmail.com>
2023-07-03 12:06:40 -0400

    documentation method

--- models/base_model.py
+++ models/base_model.py
@@ -9,20 +9,25 @@ class BaseModel:
     """class named basemodel"""
 
     def __init__(self):
+        """constructor metohod"""
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = self.created_at
 
     def updated(self):
+        """update method"""
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """string method"""
         return (f"[{self.__class__.__name}] ({self.id}) {self.__dict__}")
 
     def save(self):
+        """save updated info method"""
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """convert dictionary method"""
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
