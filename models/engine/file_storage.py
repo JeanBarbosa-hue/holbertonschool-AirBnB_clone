a13ad71cfcaac0fcc1a7146508109c7154e48822
JonathanPR24 <jonathanperez604@gmail.com>
2023-07-04 12:19:28 -0400

    Store first object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,9 +1,10 @@
 import json
+from models.base_model import BaseModel
 
 
 class FileStorage:
     """
-    Class for serializing instances to a JSON file and 
+    Class for serializing instances to a JSON file and
     deserializing JSON file to instances.
     """
 
@@ -12,34 +13,31 @@ class FileStorage:
     __objects = {}
 
     def all(self):
-        """Returns the dictionary __objects."""
-        return self.__objects
+        """Returns the dictionary __objects"""
+        return FileStorage.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id."""
-        key = obj.__class__.__name__ + '.' + obj.id
-        self.__objects[key] = obj
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
 
     def save(self):
-        """Serializes __objects to the JSON file (__file_path)."""
-        obj_dict = {key: obj.to_dict() for key, obj in self.__objects.items()}
-        with open(self.__file_path, 'w') as file:
+        """Serializes __objects to the JSON file (path: __file_path)"""
+        obj_dict = {}
+        for key, obj in FileStorage.__objects.items():
+            obj_dict[key] = obj.to_dict()
+        with open(FileStorage.__file_path, "w") as file:
             json.dump(obj_dict, file)
 
     def reload(self):
-        """
-        Deserializes the JSON file (__file_path) \
-                to __objects (if the file exists).
-        """
+        """Deserializes the JSON file to __objects"""
         try:
-            with open(self.__file_path, 'r') as file:
+            with open(FileStorage.__file_path, "r") as file:
                 obj_dict = json.load(file)
-                from models.base_model import BaseModel
                 for key, value in obj_dict.items():
-                    class_name, obj_id = key.split('.')
-                    class_ = BaseModel if class_name == 'BaseModel' else None
-                    if class_:
-                        obj = class_(**value)
-                        self.__objects[key] = obj
+                    cls_name, obj_id = key.split('.')
+                    cls = eval(cls_name)
+                    obj = cls(**value)
+                    FileStorage.__objects[key] = obj
         except FileNotFoundError:
             pass
